d754111e80d4fc598e7d8c36ac2c107211e1112d
Webb Henderson <webb.henderson@gmail.com>
2020-07-24 16:24:24 -0600

    add markdown-it and test markdown

--- app/pods/components/markdown/component.js
+++ app/pods/components/markdown/component.js
@@ -1,7 +1,16 @@
 import Component from '@ember/component';
+import MarkdownIt from 'markdown-it';
+import { computed } from '@ember/object';
+import { htmlSafe } from '@ember/string';
+
+const md = new MarkdownIt();
 
 export default Component.extend({
 
-  source: 'Hello, *markdown*!'
+  source: 'Hello, *markdown*!',
+
+  html: computed('source', function() {
+    return htmlSafe(md.render(this.source));
+  })
 
 });
