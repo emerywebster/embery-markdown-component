4c75029a055c541b4573951f8e85fdef74414ec7
Webb Henderson <webb.henderson@gmail.com>
2020-07-24 16:34:21 -0600

    add dedent

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -1,7 +1,26 @@
 import Controller from '@ember/controller';
+import dedent from 'dedent';
 
 export default Controller.extend({
   
-  blogPost: `# Some markdown`
+  blogPost: dedent`
+    # Example markdown
+
+    This is an example
+
+    We'll cover **three** steps:
+
+      1. Step 1
+      2. Step 2
+      3. Step 3
+
+    ## Install
+
+    \`\`\`bash
+    npm install -g ember-cli
+    \`\`\`
+
+    That's about it!
+  `
 
 });
