a5c469c27361bf79387c4a1d612a21272e794788
Webb Henderson <webb.henderson@gmail.com>
2020-07-24 16:39:37 -0600

    add highlight.js

--- app/pods/components/markdown/component.js
+++ app/pods/components/markdown/component.js
@@ -1,9 +1,20 @@
 import Component from '@ember/component';
 import MarkdownIt from 'markdown-it';
+import hljs from 'highlight.js';
 import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/string';
 
-const md = new MarkdownIt();
+const md = new MarkdownIt({
+  highlight(code, lang) {
+    if (hljs.getLanguage(lang)) {
+    return hljs.highlight(lang, code).value
+    }
+    else {
+      return code;
+    }
+  }
+
+});
 
 export default Component.extend({
 
